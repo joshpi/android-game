f86ae55292876a34bfc9648c9f2fcb754e0e3142
joshpi <josh.pi@gmail.com>
2013-08-18 16:55:39 -0400

    Added text to game view
    Added the default string "more pew pew less qq" on the game screen

--- AndroidGame/src/com/game/android_game/GameActivity.java
+++ AndroidGame/src/com/game/android_game/GameActivity.java
@@ -1,7 +1,12 @@
 package com.game.android_game;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class GameActivity extends Activity {
-
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game);
+    }
 }

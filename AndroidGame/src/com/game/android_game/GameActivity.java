0b110d7d9c5ca400dd77143f4c5d967cce018130
joshpi <josh.pi@gmail.com>
2013-08-25 18:15:31 -0400

    Updated project properties

--- AndroidGame/src/com/game/android_game/GameActivity.java
+++ AndroidGame/src/com/game/android_game/GameActivity.java
@@ -2,13 +2,46 @@ package com.game.android_game;
 
 import android.app.Activity;
 import android.os.Bundle;
+import org.andengine.*;
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.entity.scene.Scene;
+import org.andengine.ui.activity.BaseGameActivity;
 
-public class GameActivity extends Activity {
+public class GameActivity extends BaseGameActivity {
+
+	@Override
+	public EngineOptions onCreateEngineOptions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void onCreateResources(
+			OnCreateResourcesCallback pOnCreateResourcesCallback)
+			throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onCreateScene(OnCreateSceneCallback pOnCreateSceneCallback)
+			throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onPopulateScene(Scene pScene,
+			OnPopulateSceneCallback pOnPopulateSceneCallback) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
 	//Sprite userSprite = new Sprite(x, y, textureRegion, vertexBufferObjectManager);
-	
+	/*
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game);
     }
+    */
 }
